681027a0e2d9ad9b4ffc7eaaf8403f2106e1caa4
jtaaa <joshuatallum@gmail.com>
2021-07-25 16:41:32 -0400

    Update web resume link

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,4 +3,4 @@ export const TOP_SECTION_PADDING = 72
 export const SECTION_PADDING = 24
 export const GITHUB_PROFILE_URL = "https://github.com/jtaaa"
 export const TWITTER_PROFILE_URL = "https://twitter.com/allum_"
-export const WEB_RESUME_URL = "https://jallum.xyz/web-resume"
+export const WEB_RESUME_URL = "https://resume.jallum.xyz"
