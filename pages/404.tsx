b4677a9ceec684ae815025308e4fd5f80cf66104
jtaaa <joshuatallum@gmail.com>
2021-08-01 10:54:06 -0400

    Add title and metadescription for 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import RoundContainer from "../components/RoundContainer"
 import Section from "../components/Section"
 import Text from "../components/Text"
@@ -9,23 +10,32 @@ import Link from "../components/Link"
 
 const Error404Page = () => {
   return (
-    <CenterContainer>
-      <Section>
-        <RoundContainer
-          width={LARGE_IMAGE_RADIUS}
-          height={LARGE_IMAGE_RADIUS}
-          margin={["0 auto", 0]}
-        >
-          <Image src={JoshLaughing} alt="Josh Laughing" placeholder="blur" />
-        </RoundContainer>
-        <Text variant="title" textAlign={["center", "start"]}>
-          Oops, couldn’t find that page
-        </Text>
-        <Link name="Home" href="/">
-          Head home 🏡
-        </Link>
-      </Section>
-    </CenterContainer>
+    <>
+      <Head>
+        <title>Oops! No Page Here. Joshua Allum | T&T 🇹🇹</title>
+        <meta
+          name="description"
+          content="Oops, couldn’t find that page. Don’t share this link! How did you even get here buddy?"
+        />
+      </Head>
+      <CenterContainer>
+        <Section>
+          <RoundContainer
+            width={LARGE_IMAGE_RADIUS}
+            height={LARGE_IMAGE_RADIUS}
+            margin={["0 auto", 0]}
+          >
+            <Image src={JoshLaughing} alt="Josh Laughing" placeholder="blur" />
+          </RoundContainer>
+          <Text variant="title" textAlign={["center", "start"]}>
+            Oops, couldn’t find that page
+          </Text>
+          <Link name="Home" href="/">
+            Head home 🏡
+          </Link>
+        </Section>
+      </CenterContainer>
+    </>
   )
 }
 
